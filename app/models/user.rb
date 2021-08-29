f72eaa060a60b1b578880bf575dd0cd0d5cfef4d
napa1215 <napa1215@icloud.com>
2021-08-29 12:06:51 +0900

    third_commit修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true do
+  with_options presence: true,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."} do
     # ひらがな、カタカナ、漢字のみ許可する
-    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
-    validates :family_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :first_name
+    validates :family_name
+  end
     # カタカナのみ許可する
-    validates :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :family_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+  with_options presence: true,format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."} do
+    validates :first_name_kana 
+    validates :family_name_kana
+  end
 
     # 半角英字数字のみ許可する
-    validates :password, format: {with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+    validates :password, format: {with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."} do
     end
 
-      
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :family_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :family_name_kana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :first_name
+    validates :family_name
+    validates :first_name_kana
+    validates :family_name_kana
+    validates :birthday
+  end
 end
