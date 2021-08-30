d9334f5ade4ac4a3dfe7f3b6c9eded5e84b2d6b9
napa1215 <napa1215@icloud.com>
2021-08-30 14:27:09 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,6 @@
-class ItemsController < ApplicationController   
+class ItemsController < ApplicationController  
+  private
+  
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
 end
