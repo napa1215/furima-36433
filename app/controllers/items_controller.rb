ab586f6db6d64906c5be6082e782579692f944f8
napa1215 <napa1215@icloud.com>
2021-09-02 18:33:12 +0900

    コート修正③

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
   def index
-    @items = Item.includes([:images]).order("created_at DESC")
+    # @items = Item.includes([:images]).order("created_at DESC")
   end
 
   def new
@@ -9,7 +9,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    #@item = Item.new(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path(@item)
     else
